5aec9a8caf1aed6cd04e8fe650df3f26d84937a0
nattonato <natsuki.tsutsui.08@gmail.com>
2022-09-06 22:49:13 +0900

    Fix: vcの順番が固定されるように修正
    今までのバージョンだとvcの順番がごちゃごちゃになってしまうが固定されるように修正
    また、テストが行いやすいように鯖ごとにbotや定数などを分けた
    
    #test

--- launcher.py
+++ launcher.py
@@ -1,8 +1,12 @@
 import discord
+import const
 import os
+import config
 
-ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
-BASE_CATEGORY_ID = 1016344180462714940
+CATEGORY_SORTED_TXT = const.CATEGORY_SORTED_TXT
+BASE_CATEGORY_ID = const.BASE_CATEGORY_ID
+ACCESS_TOKEN = const.ACCESS_TOKEN
+GUILD_ID = const.GUILD_ID
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
@@ -64,9 +68,31 @@ async def on_voice_state_update(member,before,after):
                     send_message_content = "```" + member.display_name + "が「"+ before.channel.name + "」から切断されました。```"
                     print(send_message_content)
                     if before.channel.category.id != BASE_CATEGORY_ID:
-                        base_channel = client.get_channel(BASE_CATEGORY_ID)
-                        base_position = base_channel.position
-                        await before.channel.category.edit(position=base_position+1)
+                        category_sorted = []
+                        # ソート済みのカテゴリーリストを取得
+                        with open(CATEGORY_SORTED_TXT, "r") as f:
+                            for line in f:
+                                category_sorted.append(int(line.strip()))
+                        category_sort_dict = dict()
+
+                        #カテゴリの順番を扱いやすいよう辞書に登録
+                        for category_position, category_id in enumerate(category_sorted):
+                            category_sort_dict[category_id] = category_position
+
+                        #カテゴリの元のpositionより大きい値のpositionが出現したらそこに挿入
+                        guild = client.get_guild(GUILD_ID)
+                        category_list = [[category.id, category.position] for category in guild.categories]
+                        print(category_list)
+                        category_base_flag = False
+                        for [category_id, category_position] in category_list:
+                            try :
+                                if category_base_flag and category_sort_dict[category_id] > category_sort_dict[before.channel.category.id]:
+                                    await before.channel.category.edit(position=category_position)
+                            except KeyError:
+                                await before.channel.category.edit(position=len(category_list)-1)
+                            if category_id == BASE_CATEGORY_ID:
+                                category_base_flag = True
+
         except AttributeError:
             pass
 
